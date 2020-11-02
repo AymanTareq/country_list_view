075cf45e60d2075beeb32d12ab217ec761ce151e
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-11-02 14:11:00 +0600

    Created a class in models.py named country_info and add some attributes

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class country_info(models.Model):
+    country_name = models.CharField(max_length=25)
+    short_description = models.CharField(max_length=256)
+    wiki_link = models.URLField(help_text='Provide a link to know more...')
+    country_flag = models.ImageField()
