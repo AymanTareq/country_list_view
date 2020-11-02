a27b73a96e464b5c4a6f88030f91618ba0455b66
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-11-02 20:40:42 +0600

    created simple view and a templates named index.html and everything wokrs well so far

--- main_app/models.py
+++ main_app/models.py
@@ -4,4 +4,7 @@ class country_info(models.Model):
     country_name = models.CharField(max_length=25)
     short_description = models.CharField(max_length=256)
     wiki_link = models.URLField(help_text='Provide a link to know more...')
-    country_flag = models.ImageField()
+    country_flag = models.ImageField(upload_to='images/',null=True)
+
+    def __str__(self):
+        return self.name + ": " + str(self.country_flag)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Success')
+    return render(request,'main_app/index.html')
+
+
+
+
+
+# This is from app2 application
+# from django.shortcuts import render, redirect
+# from .models import Image
+# from .forms import ImageForm
+
+# def showimage(request):
+
+#     lastimage= Image.objects.last()
+
+#     imagefile= lastimage.imagefile
+
+
+#     form= ImageForm(request.POST or None, request.FILES or None)
+#     if request.method == 'POST':
+            
+#         if form.is_valid():
+#             form.save()
+#         return redirect('img')
+#     else:
+#         pass
+
+            
+#         context= {'imagefile': imagefile,
+#                 'form': form
+#                 }
+    
+      
+#     return render(request, 'app2/image.html', context)
