248f0fa12419933fcf20599fd22f6da3bd0c2655
AymanTareq.97 <tareq.ctec02@gmail.com>
2020-11-04 12:09:18 +0600

    fix the problem dj warning about forward slash and now everything wokrs well

--- country_list_view/urls.py
+++ country_list_view/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('main_app',include('main_app.urls')),
+    path('main_app/',include('main_app.urls')),
     path('',include('main_app.urls')),
 ]+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
